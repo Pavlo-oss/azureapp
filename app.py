8e37346555cee29b7c44b4a06f9fe13d1a38fa5b
Pavlo-oss <pasha.amon@gmail.com>
2025-09-30 10:27:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask
 app = Flask(__name__)
 @app.route("/")
-def hello():
-  return "Hello students"
+def weather():
+  return """
+  <h1>Прогноз погоди</h1>
+  <ul>
+    <li>Київ: 18°C, сонячно</li>
+    <li>Львів: 16°C, хмарно</li>
+    <li>Одеса: 20°C, дощ</li>
+  </ul>
+  """
 if __name__ == "__main__":
-  # Порт вказується через змінну середовища для Azure
   import os
   port = int(os.environ.get("PORT", 5000))
   app.run(host="0.0.0.0", port=port)
